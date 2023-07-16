43625ca98262fba66a9bfb065a1d4ef08ef9c5ff
ananthi <Maheshwaran2328@gmail.com>
2023-07-16 21:44:29 +0530

    header navbar portion added

--- src/js/index.js
+++ src/js/index.js
@@ -11,7 +11,25 @@ tabsContainer.addEventListener("click", (e) => {
 
     const target = e.target.getAttribute("data-target");
 
-    aboutSection.querySelector(".tab-content.active").classList.remove("active");
+    aboutSection
+      .querySelector(".tab-content.active")
+      .classList.remove("active");
     aboutSection.querySelector(target).classList.add("active");
   }
 });
+
+// Navigation Toggler
+
+const navToggler = document.querySelector(".nav-toggler");
+
+navToggler.addEventListener("click", () => {
+  hideSection();
+  toggleNavbar();
+});
+
+function hideSection() {
+  document.querySelector("section.active").classList.toggle("fade-out");
+}
+function toggleNavbar() {
+  document.querySelector(".header").classList.toggle("active");
+}
