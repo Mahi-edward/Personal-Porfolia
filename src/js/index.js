c1227189e876632ca756218536e6a5658fd006ee
ananthi <Maheshwaran2328@gmail.com>
2023-07-17 22:40:07 +0530

    header changes and circle animation done

--- src/js/index.js
+++ src/js/index.js
@@ -25,6 +25,7 @@ const navToggler = document.querySelector(".nav-toggler");
 navToggler.addEventListener("click", () => {
   hideSection();
   toggleNavbar();
+  document.body.classList.toggle("hide-scrolling");
 });
 
 function hideSection() {
@@ -33,3 +34,32 @@ function hideSection() {
 function toggleNavbar() {
   document.querySelector(".header").classList.toggle("active");
 }
+
+// Active Section
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("link-item") && !e.target.hash !== "") {
+    navToggler.classList.add("hide");
+    document.querySelector(".overlay").classList.add("active");
+    const hash = e.target.hash;
+    // console.log(hash);
+
+    if (e.target.classList.contains("nav-item")) {
+      toggleNavbar();
+    } else {
+      hideSection();
+      document.body.classList.add("hide-scrolling");
+    }
+
+    setTimeout(() => {
+      document
+        .querySelector("section.active")
+        .classList.remove("active", "fade-out");
+      document.querySelector(hash).classList.add("active");
+      window.scrollTo(0, 0);
+      document.body.classList.remove("hide-scrolling");
+      navToggler.classList.remove("hide");
+      document.querySelector(".overlay").classList.remove("active");
+    }, 500);
+  }
+});
