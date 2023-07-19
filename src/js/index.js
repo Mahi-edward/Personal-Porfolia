b7879b9d51da5e64a6ab10122578b8f3778663b5
ananthi <Maheshwaran2328@gmail.com>
2023-07-19 23:04:36 +0530

    portfolio img added, icons fontawesome added

--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,7 @@ window.addEventListener("load", () => {
   }, 600);
 });
 
+// About Tabs
 const tabsContainer = document.querySelector(".about-tabs"),
   aboutSection = document.querySelector(".about-section");
 
@@ -30,6 +31,47 @@ tabsContainer.addEventListener("click", (e) => {
     aboutSection.querySelector(target).classList.add("active");
   }
 });
+
+// PortFolio Item Details Popup
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("view-project-btn")) {
+    togglePorfolioPopup();
+    document.querySelector(".portfolio-popup").scrollTo(0, 0);
+    portfolioItemDetails(e.target.parentElement);
+  }
+});
+
+function togglePorfolioPopup() {
+  document.querySelector(".portfolio-popup").classList.toggle("open");
+  document.body.classList.toggle("hide-scrolling");
+  document.querySelector("main").classList.toggle("fade-out");
+}
+
+document
+  .querySelector(".pp-close")
+  .addEventListener("click", togglePorfolioPopup);
+
+// Hide popup when clicking outside
+document.addEventListener("click", (e) => {
+  // console.log(e.target.classList);
+  if (e.target.classList.contains("pp-inner")) {
+    togglePorfolioPopup();
+  }
+});
+
+function portfolioItemDetails(portfolioItem) {
+  document.querySelector(".pp-thumbnail img").src = portfolioItem.querySelector(
+    ".portfolio-item-thumbnail img"
+  ).src;
+
+  document.querySelector(".pp-header h3").innerHTML =
+    portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+
+  document.querySelector(".pp-body").innerHTML = portfolioItem.querySelector(
+    ".portfolio-item-details"
+  ).innerHTML;
+}
 
 // Navigation Toggler
 
